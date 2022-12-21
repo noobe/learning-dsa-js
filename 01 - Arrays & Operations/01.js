b58bcfe1578963fe1f76f3bb8810dba926c8c1e2
Sarath <sarath.regedit@gmail.com>
2022-12-21 23:35:27 +0530

    Creating arrays in JS

---
+++ 01 - Arrays & Operations/01.js
@@ -0,0 +1,19 @@
+let arr0 = [];
+let arr1 = [1, 2, 3, 4];
+let arr2 = ['str1', 'str2', 'str3'];
+let arr3 = [true, true, false];
+let arr4 = [{ a: 1 }, { a: 2 }, { a: 3 }];
+let arr5 = [undefined, undefined];
+let arr6 = [null, null, null]
+let arr7 = [[1, 2], [2, 3], [4, 5, 6]]
+let arr8 = [1, 'str2', true, { b: 123 }, undefined, null, new Date(), (() => 1)()];
+
+console.log(arr0);
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+console.log(arr4);
+console.log(arr5);
+console.log(arr6);
+console.log(arr7);
+console.log(arr8);
