5d2f814fe0e31b792487236e670e52083bdb7829
Sarath <sarath.regedit@gmail.com>
2022-12-29 22:33:40 +0530

    new and object.assign

--- 02 - Objects or hash tables/01.js
+++ 02 - Objects or hash tables/01.js
@@ -5,3 +5,13 @@ let obj = {
 
 // Creating using Object.create() method
 const obj2 = Object.create(obj);
+
+// Creating objects from classes using new
+function Person(fname, lname) {
+  this.firstName = fname;
+  this.lastName = lname;
+}
+const obj3 = new Person('testFirstNameOne', 'testLastNameOne');
+
+// Create using Object.assign() method
+const obj4 = Object.assign({}, obj2, obj3);
