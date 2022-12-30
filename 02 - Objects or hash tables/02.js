033f157570aedb16f14d76b21f63cb5f6d231a61
Sarath <sarath.regedit@gmail.com>
2022-12-30 21:57:38 +0530

    Object keys and values

---
+++ 02 - Objects or hash tables/02.js
@@ -0,0 +1,11 @@
+let obj = {
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3',
+}
+
+// Get a list of all keys in the object
+Object.keys(obj);
+
+// Get a list of all values in the object
+Object.values(obj);
